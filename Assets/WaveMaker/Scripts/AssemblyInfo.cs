e0e218876951b5dd6dbfee84553107d393573b24
sohyeon412 <162390471+sohyeon412@users.noreply.github.com>
2025-04-13 20:28:15 +0900

    Delete Assets directory

--- Assets/WaveMaker/Scripts/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿// NOTE: This allows the test and debug classes access internal type functions and variables on this assembly
-using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("WaveMaker.Tests.Editor")]
-[assembly: InternalsVisibleTo("WaveMaker.Editor")]
